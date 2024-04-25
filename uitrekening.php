0659b11933b7ce7393f1dae182d9308fff2cc32e
damien <97094049@st.deltion.nl>
2024-04-25 16:26:34 +0200

    edit
    edit

---
+++ uitrekening.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tafels</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+<center>
+    <div class="phpindex-text">
+        <h1>Tafles berekenen.</h1>
+        <p>Hier kunt u tafels berekenen!</p>
+    </div>
+  
+<div class="submit-button">    
+    <form action="index.html">
+        <input type="submit" value="Opnieuw iets berekenen">
+    </form>
+</div>
+    
+    <div class="phpcode">
+        <?php
+
+            $tafel = $_POST['tafel'];
+            $van = $_POST['vanaf'];
+            $tm = $_POST['tm'];
+        
+            $afstand = abs($van - $tm);
+
+            if($afstand > 20) {
+                echo "De afstand is groter dan 20. Voer alstublieft een getal kleiner of gelijk aan 20.";
+            } else {
+                // Check if $van is greater than $tm and swap them if necessary
+                if($van > $tm) {
+                    $temp = $van;
+                    $van = $tm;
+                    $tm = $temp;
+                }
+                
+                echo "Hier is uw resultaat! <br><br>";
+
+                for($i=$van; $i<=$tm; $i++){
+                    $uitkomst = $i * $tafel;
+
+                    echo "$i keer $tafel is $uitkomst <br>";
+                }
+            }
+
+        ?>
+    </div>
+        <br><br><br>
+    
+</center>
+
+
+
+</body>
+</html>
